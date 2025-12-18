99421c087cc1f339eb4811885580a4b8ef3face4
ebenhaezer19 <krisoprasebenhaeezer@gmail.com>
2025-12-19 04:32:05 +0700

    moodle url update for demo 4

--- proxy/config.py
+++ proxy/config.py
@@ -3,9 +3,9 @@ Configuration constants for the Moodle proxy service.
 """
 
 # Target Moodle instance base URL
-# MOODLE_URL: str = "http://localhost:8998"
+MOODLE_URL: str = "http://localhost:8998"
 # MOODLE_URL: str = "http://localhost:9000"
-MOODLE_URL: str = "https://sdecdtsepas2024.gnomio.com"
+# MOODLE_URL: str = "https://sdecdtsepas2024.gnomio.com"
 
 # Port for the proxy service to listen on
 LISTEN_PORT: int = 8999
